16c2ae51fdfc98e644e3ce448970337aad34238f
Rani Abu Sultan <raniabusultan@gmail.com>
2019-03-28 19:48:32 +0300

    Querying Firestore Documents using the Firebase SDK

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import 'firebase/firestore'
+
+const config = {
+  apiKey: "AIzaSyD0-sMPDsuukbQ2V5UQjcjNbqZlliVq54g",
+  authDomain: "serverless-angular-angularfire.firebaseapp.com",
+  databaseURL: "https://serverless-angular-angularfire.firebaseio.com",
+  projectId: "serverless-angular-angularfire",
+  storageBucket: "serverless-angular-angularfire.appspot.com",
+  messagingSenderId: "422661974860"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore()
+
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
@@ -10,6 +26,9 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    db.doc('courses/1VfptzJnTyFB4gtc3uJg')
+    .get()
+    .then(snap => console.log(snap.data()));
   }
 
 }
