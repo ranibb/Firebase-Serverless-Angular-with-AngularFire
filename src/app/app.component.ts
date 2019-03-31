fd708034c1b7a9dc8d9e84224155225c1ccae25f
Rani Abu Sultan <raniabusultan@gmail.com>
2019-03-31 10:41:01 +0300

    The AngularFire Auth User Profile - login & logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+
+    // If the user is logged in, we receive the user information.
+    this.afAuth.authState.subscribe(user => console.log(user))
+
+    /**
+     * Double negation transforms the variable into a boolean. So if the user
+     * variable value is defined, then this observable is going to emit true &
+     * if the user is logged out and the user variable value is null, then this 
+     * observable is going to emit false.
+     */
+    this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn))
+  }
 
   logout() {
-
+    this.afAuth.auth.signOut();
   }
 
 }
