f38de7fc30b9a14abc3c016f3b99ee9b3545a320
Rani Abu Sultan <raniabusultan@gmail.com>
2019-04-02 23:28:38 +0300

    Deploy out application into production using firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD0-sMPDsuukbQ2V5UQjcjNbqZlliVq54g",
+    authDomain: "serverless-angular-angularfire.firebaseapp.com",
+    databaseURL: "https://serverless-angular-angularfire.firebaseio.com",
+    projectId: "serverless-angular-angularfire",
+    storageBucket: "serverless-angular-angularfire.appspot.com",
+    messagingSenderId: "422661974860"
+  }
 };
