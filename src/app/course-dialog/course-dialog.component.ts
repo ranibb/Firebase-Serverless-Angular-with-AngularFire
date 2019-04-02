259ae50c752381a6ee045c64f2a5c671f9b65392
Rani Abu Sultan <raniabusultan@gmail.com>
2019-04-03 00:16:15 +0300

    Adding firebase storage to our project and deploy + initial Implementation of file upload

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,7 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { Course } from "../model/course";
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { CoursesService } from '../services/courses.service';
-import { subscribeOn } from 'rxjs/operators';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'course-dialog',
@@ -20,7 +20,8 @@ export class CourseDialogComponent implements OnInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private coursesService: CoursesService) {
+    private coursesService: CoursesService,
+    private storage: AngularFireStorage) {
 
     this.course = course;
 
@@ -31,6 +32,20 @@ export class CourseDialogComponent implements OnInit {
       longDescription: [titles.longDescription, Validators.required]
     });
 
+  }
+
+  uploadFile(event) {
+
+    // Get a reference of the uploaded file from the event (Access the array of uploaded files).
+    const file: File = event.target.files[0];
+
+    // Define it's ditination target path.
+    const filePath = `courses/${this.course.id}/${file.name}`;
+
+    // Trigger the upload using the AngularFireStorage service .
+    const task = this.storage.upload(filePath, file);
+    task.snapshotChanges().subscribe(console.log);
+    
   }
 
   ngOnInit() {
