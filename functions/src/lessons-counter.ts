5446cc1c20bebccb05a38df65143de224bb45b15
Rani Abu Sultan <raniabusultan@gmail.com>
2019-04-09 20:43:49 +0300

    refactor

--- functions/src/lessons-counter.ts
+++ functions/src/lessons-counter.ts
@@ -16,30 +16,37 @@ export const onAddLesson = functions.firestore.document('courses/{courseId}/less
 
       console.log("Running onAddLesson trigger ...");
 
-      /**
-       * Read our course from the database and we need to increment the lessonsCount. However,
-       * we should do this in a transactional way (the value of the trigger cannot be modified
-       * while our database trigger is getting executed).
-       */
-      return db.runTransaction(
-        /**
-         * Inside the transaction body, we can read the course and modify the counter. We are going to
-         * use async await
-         */
-        async transaction => {
-          // Obtain a reference to our course document at the lesson document level 'courses/{courseId}/lessons/{lessonId}' 
-          const courseRef = snap.ref.parent.parent;
-          // receiving the course snapshot in a transactional way
-          const courseSnap = await transaction.get(courseRef);
-          // retrieve the course data itself
-          const course = courseSnap.data();
-          // create a changes object continuing the modifications that we want to do to the course object
-          const changes = { lessonsCount: course.lessonsCount + 1 };
-          // apply the changes to the database using the transaction.update method.
-          transaction.update(courseRef, changes);
-        });
+      return courseTransaction(snap, course => {
+        return { lessonsCount: course.lessonsCount + 1 }
+      })
+
     }
   );
+
+function courseTransaction(snap, callback: Function) {
+  /**
+   * Read our course from the database and we need to increment the lessonsCount. However,
+   * we should do this in a transactional way (the value of the trigger cannot be modified
+   * while our database trigger is getting executed).
+   */
+  return db.runTransaction(
+    /**
+     * Inside the transaction body, we can read the course and modify the counter. We are going to
+     * use async await
+     */
+    async transaction => {
+      // Obtain a reference to our course document at the lesson document level 'courses/{courseId}/lessons/{lessonId}' 
+      const courseRef = snap.ref.parent.parent;
+      // receiving the course snapshot in a transactional way
+      const courseSnap = await transaction.get(courseRef);
+      // retrieve the course data itself
+      const course = courseSnap.data();
+      // create a changes object continuing the modifications that we want to do to the course object
+      const changes = callback(course)
+      // apply the changes to the database using the transaction.update method.
+      transaction.update(courseRef, changes);
+    });
+}
 
 /**
  * We are expected to return the transaction promise so that the engine that is calling the
